a3571273a19f08787d090774f403600d158416e2
tomasxrp <rjosetomas6@gmail.com>
2026-04-20 12:42:40 -0400

    Funcionalidad: Agregue el endpoint para listar todos los usuarios de la base de datos

--- backend/src/controllers/UsuarioController.js
+++ backend/src/controllers/UsuarioController.js
@@ -88,9 +88,27 @@ const obtenerUsuarioPorCorreo = async (req, res) => {
   }
 };
 
+const ObtenerListaUsuarios = async (req, res) => {
+  try {
+    const { usuario } = req;
+
+    const listaUsuarios = await usuarioService.ObtenerListaUsuarios(usuario);
+
+    res.status(200).json({
+      mensaje: 'Lista de usuarios obtenida con exito',
+      usuarios: listaUsuarios,
+    });
+  } catch (error) {
+    res.status(401).json({
+      mensaje: error.message || 'Error al obtener lista de usuarios',
+    });
+  }
+};
+
 module.exports = {
   registrarUsuario,
   loginUsuario,
   eliminarUsuario,
   obtenerUsuarioPorCorreo,
+  ObtenerListaUsuarios,
 };
