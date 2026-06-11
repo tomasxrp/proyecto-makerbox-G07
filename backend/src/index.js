f1e00c77dec1c0f67b974def76be71d75bf498fc
tomasxrp <rjosetomas6@gmail.com>
2026-06-11 10:40:34 -0400

    feat: Creacion de los endpoints para grupo_curso

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ const cursoRoutes = require('./routes/CursoRoutes');
 const impresionRoutes = require('./routes/ImpresionRoutes');
 const bloqueHorarioRoutes = require('./routes/BloqueHorarioRoutes');
 const estudianteCursoRoutes = require('./routes/EstudianteCursoRoutes');
+const grupoCursoRoutes = require('./routes/GrupoCursoRoutes');
 
 const app = express();
 // Se define el puerto que se usara, si no se define se usara el puerto 3000
@@ -22,6 +23,7 @@ app.use('/api/curso', cursoRoutes);
 app.use('/api/impresion', impresionRoutes);
 app.use('/api/bloque-horario', bloqueHorarioRoutes);
 app.use('/api/estudiante-curso', estudianteCursoRoutes);
+app.use('/api/grupo-curso', grupoCursoRoutes);
 app.get('/', (req, res) => {
   res.send('Peticion GET recibida en el backend');
 });
