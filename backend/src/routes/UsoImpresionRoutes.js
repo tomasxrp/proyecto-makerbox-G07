3a9f94be037f4d3dd6db0ec75971c95d9a1079f6
Negaces <negaces01@gmail.com>
2026-06-11 21:30:32 -0400

    feat: rutas y validacion de usoImpresion

---
+++ backend/src/routes/UsoImpresionRoutes.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const { validarToken } = require('../middlewares/validarToken');
+const { validarUsoImpresion } = require('../middlewares/validarUsoImpresion');
+const usoImpresionController = require('../controllers/UsoImpresionController');
+
+const router = Router();
+
+router.post(
+  '/crear',
+  validarToken,
+  validarUsoImpresion,
+  usoImpresionController.crearUsoImpresion
+);
+router.get('/', validarToken, usoImpresionController.obtenerUsosImpresion);
+router.get(
+  '/:usoImpresionId',
+  validarToken,
+  usoImpresionController.obtenerUsoImpresionPorId
+);
+router.put(
+  '/actualizar/:usoImpresionId',
+  validarToken,
+  usoImpresionController.actualizarUsoImpresion
+);
+router.delete(
+  '/eliminar/:usoImpresionId',
+  validarToken,
+  usoImpresionController.eliminarUsoImpresion
+);
+
+module.exports = router;
